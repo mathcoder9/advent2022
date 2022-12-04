6a7d471a83d5dcb5e51f521c86f60b3d11c00455
Sayon <sayon_u@hotmail.co.uk>
2022-12-04 14:34:36 +0000

    solutions for day 4

---
+++ src/solutions/day4/day4.py
@@ -0,0 +1,48 @@
+class Solution:
+
+    def __init__(self) -> None:
+        self.sol1 = 0
+        self.sol2 = 0
+        self.init_data()
+        self.solve()
+
+    def init_data(self):
+        with open("../../data/day4.txt") as file:
+            self.data = file.read().split("\n")
+
+    def convert_data(self, intervalData):
+        intervals = intervalData.split(",")
+        pairs = []
+        for interval in intervals:
+            temp = interval.split("-")
+            pairs.append([int(temp[0]), int(temp[1])])
+
+        return pairs
+
+    def check_overlap(self, pairs):
+        pair1, pair2 = pairs
+        return not min(pair1[1], pair2[1]) < max(pair1[0], pair2[0])
+
+    def check_contains(self, pairs):
+        pair1, pair2 = pairs
+        if pair1[0] == pair2[0]:
+            return True
+        elif pair1[0] < pair2[0]:
+            return pair2[1] <= pair1[1]
+        else:
+            return pair1[1] <= pair2[1]
+
+    def solve(self):
+        for item in self.data:
+            pairs = self.convert_data(item)
+            if self.check_contains(pairs):
+                self.sol1 += 1
+            if self.check_overlap(pairs):
+                self.sol2 += 1
+
+    def get_solution(self):
+        return (self.sol1, self.sol2)
+
+
+solution = Solution()
+print(solution.get_solution())
